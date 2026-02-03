e3c8134060e09dfdeba7a8a0d226d660d1a8e145
soty <websoty@gmail.com>
2026-02-03 16:42:37 +0200

    feat: menu nav

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import "dotenv/config";
 import { Telegraf } from "telegraf";
 import { handleStart, handleAction } from "./engine/engine.js"
+import { botTree } from "./tree/botTree.js";
+import { renderNode } from "./engine/render.js";
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN!);
 
 bot.start(handleStart);
 bot.on("callback_query", handleAction);
+
+bot.command("about", (ctx) => {
+  renderNode(ctx, botTree["about"]!);
+});
+
+bot.command("join", (ctx) => {
+  renderNode(ctx, botTree["join"]!);
+});
+
+bot.command("vacancies", (ctx) => {
+  renderNode(ctx, botTree["all_vacancies"]!);
+});
+
+bot.command("units", (ctx) => {
+  renderNode(ctx, botTree["units"]!);
+});
+
+bot.command("help", (ctx) => {
+  renderNode(ctx, botTree["contact_rc"]!);
+});
 
 
 bot.launch();
